2bd6ddbe8824fe523441e8aa8e95a924e8d81a69
Brian Hays <bhays@vpsland.com>
2011-05-29 03:05:46 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '2fa6f705ac949c054b5483eaf75e192f218cf1742537686f0dc0f307da5bd386ff4055f995126183a2c1ffcf16b493a023e99d74a56b70e0239c9727169e0bcf'
